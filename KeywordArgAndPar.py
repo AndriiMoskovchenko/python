d599d56e146f548f216e2517e432e1155afe2a20
AndriiMoskovchenko <55872286+AndriiMoskovchenko@users.noreply.github.com>
2020-08-04 08:07:59 +0200

    Add files via upload

--- KeywordArgAndPar.py
+++ KeywordArgAndPar.py
@@ -3,3 +3,15 @@ def about(name, age, likes):
 	return sentence
 
 about(age=23, name = "John", likes = "Potato")
+
+##Default value. Just add it in arguments. Default value have to go last ALWAYS.
+##U can't just add it in chaotic way.
+
+#arguments -> what you see in def (age, name, likes)
+#parameters -> what you pass when call def (John, 23, Potato)
+def aboutWdefVal(name, age, likes="himself"):
+	sentence = "Meet {}! He's {} years old and he likes {}".format(name, age, likes)
+	return sentence
+
+
+    
