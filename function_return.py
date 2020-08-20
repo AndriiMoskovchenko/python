bfb58103bc10b678cd82386cb310a715cf2ad458
AndriiMoskovchenko <55872286+AndriiMoskovchenko@users.noreply.github.com>
2020-08-20 16:42:19 +0300

    Add files via upload

---
+++ function_return.py
@@ -0,0 +1,13 @@
+def maximum(x,y):
+    if x>y:
+        return x
+    elif y==x:
+        return 'The numbers are equal'
+    else:
+        return y
+    
+print(maximum(2,3))
+
+
+def some_function():
+    return None
